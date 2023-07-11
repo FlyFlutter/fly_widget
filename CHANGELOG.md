269e2eff7225623ad3898d510e20128e4695979f
张博 <zbjumper@gmail.com>
2023-07-11 11:01:00 +0800

    拆分辅助色

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.0.6
+
+- 辅助色拆分为 6 个，分别为 A、B、C、D、E、F
+
 # 0.0.5
 
 - 给 FlyPositiveButtonExtension 增加特殊的 disable color
--- lib/src/extensions/color.dart
+++ lib/src/extensions/color.dart
@@ -5,18 +5,38 @@ class FlyColorExtension extends ThemeExtension<FlyColorExtension> {
   ///
   const FlyColorExtension({
     this.primaryColor,
-    this.assistantColor,
+    this.assistantColorA,
+    this.assistantColorB,
+    this.assistantColorC,
+    this.assistantColorD,
+    this.assistantColorE,
+    this.assistantColorF,
     this.dangerousColor,
     this.blackColor,
     this.greyColor,
     this.backgroundColor,
   });
 
-  /// 主色
+  /// 品牌色
   final Color? primaryColor;
 
-  /// 辅助色
-  final Color? assistantColor;
+  /// 辅助色 A
+  final Color? assistantColorA;
+
+  /// 辅助色 B
+  final Color? assistantColorB;
+
+  /// 辅助色 C
+  final Color? assistantColorC;
+
+  /// 辅助色 D
+  final Color? assistantColorD;
+
+  /// 辅助色 E
+  final Color? assistantColorE;
+
+  /// 辅助色 F
+  final Color? assistantColorF;
 
   /// 危险的颜色
   final Color? dangerousColor;
@@ -33,29 +53,15 @@ class FlyColorExtension extends ThemeExtension<FlyColorExtension> {
   final Color? backgroundColor;
 
   @override
-  FlyColorExtension copyWith({
-    Color? primaryColor,
-    Color? assistantColor,
-    Color? dangerousColor,
-    Color? blackColor,
-    Color? greyColor,
-    Color? backgroundColor,
-  }) {
-    return FlyColorExtension(
-      primaryColor: primaryColor ?? this.primaryColor,
-      assistantColor: assistantColor ?? this.assistantColor,
-      dangerousColor: dangerousColor ?? this.dangerousColor,
-      blackColor: blackColor ?? this.blackColor,
-      greyColor: greyColor ?? this.greyColor,
-      backgroundColor: backgroundColor ?? this.backgroundColor,
-    );
-  }
-
-  @override
   FlyColorExtension lerp(FlyColorExtension? other, double t) {
     return FlyColorExtension(
       primaryColor: Color.lerp(primaryColor, other?.primaryColor, t),
-      assistantColor: Color.lerp(assistantColor, other?.assistantColor, t),
+      assistantColorA: Color.lerp(assistantColorA, other?.assistantColorA, t),
+      assistantColorB: Color.lerp(assistantColorB, other?.assistantColorB, t),
+      assistantColorC: Color.lerp(assistantColorC, other?.assistantColorC, t),
+      assistantColorD: Color.lerp(assistantColorD, other?.assistantColorD, t),
+      assistantColorE: Color.lerp(assistantColorE, other?.assistantColorE, t),
+      assistantColorF: Color.lerp(assistantColorF, other?.assistantColorF, t),
       dangerousColor: Color.lerp(dangerousColor, other?.dangerousColor, t),
       blackColor: Color.lerp(blackColor, other?.blackColor, t),
       greyColor: Color.lerp(greyColor, other?.greyColor, t),
@@ -63,4 +69,32 @@ class FlyColorExtension extends ThemeExtension<FlyColorExtension> {
     );
   }
 
+  @override
+  FlyColorExtension copyWith({
+    Color? primaryColor,
+    Color? assistantColorA,
+    Color? assistantColorB,
+    Color? assistantColorC,
+    Color? assistantColorD,
+    Color? assistantColorE,
+    Color? assistantColorF,
+    Color? dangerousColor,
+    Color? blackColor,
+    Color? greyColor,
+    Color? backgroundColor,
+  }) {
+    return FlyColorExtension(
+      primaryColor: primaryColor ?? this.primaryColor,
+      assistantColorA: assistantColorA ?? this.assistantColorA,
+      assistantColorB: assistantColorB ?? this.assistantColorB,
+      assistantColorC: assistantColorC ?? this.assistantColorC,
+      assistantColorD: assistantColorD ?? this.assistantColorD,
+      assistantColorE: assistantColorE ?? this.assistantColorE,
+      assistantColorF: assistantColorF ?? this.assistantColorF,
+      dangerousColor: dangerousColor ?? this.dangerousColor,
+      blackColor: blackColor ?? this.blackColor,
+      greyColor: greyColor ?? this.greyColor,
+      backgroundColor: backgroundColor ?? this.backgroundColor,
+    );
+  }
 }
